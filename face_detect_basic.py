f5d291e6810a987114262f23c8da3161ec8c160c
prateek-ml <prateekb1912@gmail.com>
2021-07-10 09:13:34 +0530

    Decrease frame rate to around 50

--- face_detect_basic.py
+++ face_detect_basic.py
@@ -4,18 +4,17 @@ import time
 
 cap = cv2.VideoCapture("friends.mp4")
 
-
 pTime = 0 
 while True:
     _, img = cap.read()
-
-    cv2.imshow("Video", img)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f"FPS: {fps}", (20, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 1)
+    cv2.putText(img, f"FPS: {int(fps)}", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+    
+    cv2.imshow("Video", img)
 
-    if cv2.waitKey(1) == 27:
+    if cv2.waitKey(20) == 27:
         break
