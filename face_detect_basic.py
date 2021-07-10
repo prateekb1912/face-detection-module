fddbafc8ce1b361a686fd2b99d8cfa30f001afa9
prateek-ml <prateekb1912@gmail.com>
2021-07-10 09:39:31 +0530

    Add detection accuracy and bounding box

--- face_detect_basic.py
+++ face_detect_basic.py
@@ -3,12 +3,12 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture("friends.mp4")
-pTime = 0 
+pTime = 0
 
 mpFaceDetect = mp.solutions.face_detection
 mpDraw = mp.solutions.drawing_utils
 
-faceDetection = mpFaceDetect.FaceDetection()
+faceDetection = mpFaceDetect.FaceDetection(0.75)
 
 while True:
     _, img = cap.read()
@@ -17,16 +17,21 @@ while True:
     results = faceDetection.process(imgRGB)
 
     if results.detections:
-        for idx,detection in enumerate(results.detections):
+        for idx, detection in enumerate(results.detections):
+            bBoxC = detection.location_data.relative_bounding_box
+
+            ih, iw, ic = img.shape
+            bBox = int(bBoxC.xmin * iw), int(bBoxC.ymin * ih),\
+                int(bBoxC.width * iw), int(bBoxC.height*ih)
             
-            cv2.putText(img, f"{int(detection.score[0]*100)} %", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-
-
+            cv2.rectangle(img, bBox, (127, 128, 0), 2)    
+            cv2.putText(img, f"{int(detection.score[0]*100)} %",
+                        (bBox[0], bBox[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (127, 128, 0), 3)
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
-    
+
     cv2.imshow("Video", img)
 
-    if cv2.waitKey(15) == 27:
+    if cv2.waitKey(12) == 27:
         break
